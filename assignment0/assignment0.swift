0d11580fd01c83858f51e3d246befcc5ca1737a9
peng-u-0807 <penguin0807@snu.ac.kr>
2024-09-12 01:08:59 +0900

    Assignment 0 skeleton code

---
+++ assignment0/assignment0.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private lazy var seminarLabel: UILabel = {
+        let label = UILabel()
+        // TODO
+        return label
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton()
+        // TODO
+        return button
+    }()
+    
+    private lazy var idTextField: UITextField = {
+        let textField = UITextField()
+        // TODO
+        return textField
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        // TODO
+        return textField
+    }()
+    
+    private lazy var textFieldStackView: UIStackView = {
+        let stackView = UIStackView()
+        // TODO
+        return stackView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // TODO
+    }
+    
+    // TODO
+}
+
+extension ViewController: UITextFieldDelegate {
+    // TODO
+}
